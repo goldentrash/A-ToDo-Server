cd214a3d3b0a40f36c26f010d2842364ef0ac193
white piano <startoflateness@gmail.com>
2023-06-19 11:08:02 +0900

    get todos api 구현

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+exports.asyncHandlerWrapper = (asyncRouteHandler) => {
+  return async (req, res, next) => {
+    try {
+      return await asyncRouteHandler(req, res, next);
+    } catch (err) {
+      return next(err);
+    }
+  };
+};
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+exports.contentNegotiator = (contentTypes) => (req, res, next) => {
+  if (!req.accepts(contentTypes))
+    return res.status(406).json({
+      message: 'Not Acceptable',
+      acceptables: contentTypes,
+    });
+  else return next();
+};
 
-module.exports = router;
+exports.methodNotAllowedHandler = (allowedMethods) => (req, res, next) => {
+  return res.status(406).set('Allow', allowedMethods.join(', ')).json({
+    message: 'Method Not Allowed',
+  });
+};
