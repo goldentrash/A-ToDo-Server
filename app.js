7fe2a0aeca739305dceee42b2415abf625bd29b3
white piano <startoflateness@gmail.com>
2023-06-27 23:55:27 +0900

    debug module 활용

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
+const debug = require('debug');
+const errStream = debug('a-todo:error');
+const logStream = debug('a-todo:log');
+logStream.log = console.log.bind(console);
+
 const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
 
 const app = express();
 
-app.use(logger(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
+app.use(
+  logger('dev', { stream: { write: (log) => logStream(log.trimEnd()) } })
+);
 app.use(express.json());
 
 app.use('/todos', require('./routes/todo'));
@@ -20,8 +27,20 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  err.status = err.status || 500;
+
+  if (err.status >= 500)
+    errStream('%O', {
+      err: `${err.status} ${err.message}`,
+      request: {
+        startLine: `${req.method} ${req.originalUrl} ${req.protocol}`,
+        headers: req.headers,
+        body: req.body,
+      },
+    });
+
   return res
-    .status(err.status || 500)
+    .status(err.status)
     .json({ message: err.message || 'Internal Server Error' });
 });
 
