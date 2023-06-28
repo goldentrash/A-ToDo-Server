03b3a5bcca0a61f8c254863837063e96d3de9c78
white piano <startoflateness@gmail.com>
2023-06-28 11:22:43 +0900

    update memo 구현, db schema 변경, 문서 가독성 개선

--- model/done.js
+++ model/done.js
@@ -1,11 +1,11 @@
 const { pool, createDatabaseError } = require('./index');
 
 const done = {
-  add(id, memo) {
-    const sql = `INSERT INTO done(id, memo) VALUES(?, ?);`;
+  add(id) {
+    const sql = `INSERT INTO done(id) VALUES(?);`;
 
     return new Promise((resolve, reject) => {
-      pool.query(sql, [id, memo], (err, results, fields) => {
+      pool.query(sql, [id], (err, results, fields) => {
         if (err) return reject(createDatabaseError(err));
 
         return resolve();
