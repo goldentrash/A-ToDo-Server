8456593e5ef0c0828abf3453dbd08d25936b0c68
white piano <startoflateness@gmail.com>
2023-07-06 10:47:54 +0900

    get list가 이제 마감기한 순으로 정렬

--- model/todo.js
+++ model/todo.js
@@ -3,10 +3,23 @@ const { pool, createDatabaseError } = require('./index');
 const todo = {
   get() {
     const sql = `
-    SELECT id, content, deadline FROM todo 
-    WHERE NOT EXISTS(
-      SELECT id FROM doing WHERE doing.id = todo.id
-    );`;
+    SELECT
+      id,
+      content,
+      deadline
+    FROM
+      todo
+    WHERE
+      NOT EXISTS(
+        SELECT
+          id
+        FROM
+          doing
+        WHERE
+          doing.id = todo.id
+      )
+    ORDER BY
+      deadline;`;
 
     return new Promise((resolve, reject) => {
       pool.query(sql, (err, results, fields) => {
