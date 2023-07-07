ee1f40983eb0e28112cf5075b399495138b1e85c
white piano <startoflateness@gmail.com>
2023-07-07 16:37:42 +0900

    typescript 적용

--- model/index.ts
+++ model/index.ts
@@ -1,14 +1,15 @@
-const mysql = require('mysql');
-const createError = require('http-errors');
+import mysql from 'mysql';
+import createError from 'http-errors';
+import type { MysqlError } from 'mysql';
 
-exports.pool = mysql.createPool({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: 'a_todo',
 });
 
-const userErrors = [
+const userErrors: MysqlError['code'][] = [
   'ER_DUP_ENTRY',
   'ER_NO_REFERENCED_ROW_2',
   'ER_TRUNCATED_WRONG_VALUE',
@@ -16,7 +17,7 @@ const userErrors = [
   'ER_BAD_NULL_ERROR',
 ];
 
-exports.createDatabaseError = (err) =>
+export const createDatabaseError = (err: MysqlError) =>
   userErrors.includes(err.code)
     ? createError(400, 'Bad Request')
     : createError(500, 'database error');
---
+++ routes/todo.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import {
+  asyncHandlerWrapper,
+  genContentNegotiator,
+  genMethodNotAllowedHandler,
+} from 'routes/index';
+import todo from 'model/todo';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(
+    genContentNegotiator(['json']),
+    asyncHandlerWrapper(async (_req, res, _next) => {
+      return res
+        .status(200)
+        .json({ message: 'Query Accepted', data: await todo.getAll() });
+    })
+  )
+  .post(
+    genContentNegotiator(['json']),
+    asyncHandlerWrapper(async (req, res, _next) => {
+      const { content, deadline } = req.body;
+      await todo.add(content, deadline);
+      return res.status(201).json({ message: 'todo Created' });
+    })
+  )
+  .all(genMethodNotAllowedHandler(['GET', 'POST']));
+
+export default router;
