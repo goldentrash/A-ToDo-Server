03f9116415231edfe3e78253b27249842f0e89bb
white piano <startoflateness@gmail.com>
2023-08-04 10:58:21 +0900

    config github action

--- script/init_db.sh
+++ script/init_db.sh
@@ -4,4 +4,4 @@
 export $(grep --invert-match '^#' .env | xargs --delimiter '\n')
 
 # init db
-mysql --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD < a_todo.sql
+mysql --host=$DB_HOST --user=root --password=$DB_ROOT_PASSWORD < a_todo.sql
