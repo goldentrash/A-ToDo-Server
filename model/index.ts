09d974a76f24811c6f1b3d9dc2e352c5cf5d166e
white piano <startoflateness@gmail.com>
2023-08-06 12:55:51 +0900

    db localhost loopback 수정

--- model/index.ts
+++ model/index.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST ?? "localhost",
+  host: process.env.DB_HOST ?? "127.0.0.1",
   user: process.env.DB_USER ?? "root",
   password: process.env.DB_USER
     ? process.env.DB_PASSWORD
