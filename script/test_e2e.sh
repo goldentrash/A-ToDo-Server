c81e23a3c41ea53f05a677e49c5e6efc4a847767
white piano <startoflateness@gmail.com>
2023-08-07 17:51:12 +0900

    가독성 향상

--- script/test_e2e.sh
+++ script/test_e2e.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 # run server
-yarn build:dist
-node ./bin/www &
+yarn build:dist && node ./bin/www &
 PID=$!
 
 # run test
-yarn newman run -e test/local.postman_environment.json -d test/variables.json "test/functionality test.postman_collection.json"
+yarn newman run \
+  -e test/local.postman_environment.json \
+  -d test/variables.json \
+  "test/functionality test.postman_collection.json"
 TEST_RET=$?
 
 # exit server
