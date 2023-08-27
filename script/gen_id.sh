21f8a3615b1df10ee80922c8a9a4cda59d627de6
White Piano <startoflateness@gmail.com>
2023-08-27 14:50:26 +0900

    Config/profiling (#22)
    * script 개편
    
    script 개편
    
    * log 개편

---
+++ script/gen_id.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# output path
+CSV=./artillery/id.csv
+
+# truncate output
+echo truncate $CSV
+echo -n > $CSV
+
+# gen IDs
+echo generate from user1 to user$1
+for i in $(seq 1 $1); do
+  echo user$i >> $CSV
+done
