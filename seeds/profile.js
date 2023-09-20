c5e7fbb363509f8cfe1a02777a6dcebe6c68814f
white piano <startoflateness@gmail.com>
2023-09-20 17:12:09 +0900

    profile seed 정비

--- seeds/profile.js
+++ seeds/profile.js
@@ -1,8 +1,12 @@
 const bcrypt = require("bcrypt");
-const hashed_password = bcrypt.hashSync(
-  "password",
-  parseInt(process.env.SALT_ROUND)
-);
+
+if (typeof process.env.SALT_ROUND != "string")
+  throw Error("there's no SALT_ROUND");
+const SALT_ROUND = parseInt(process.env.SALT_ROUND);
+
+if (typeof process.env.NUM_OF_VUSER != "string")
+  throw Error("there's no NUM_OF_VUSER");
+const NUM_OF_VUSER = parseInt(process.env.NUM_OF_VUSER);
 
 /**
  * @param { import("knex").Knex } knex
@@ -13,9 +17,11 @@ exports.seed = async (knex) => {
   await knex("task").truncate();
   await knex("user").del();
 
-  for (let i = 1; i <= parseInt(process.env.NUM_OF_VUSER); i++)
-    await knex("user").insert({
-      id: `user${i}`,
-      hashed_password,
-    });
+  // make Virtual Users
+  const hashed_password = bcrypt.hashSync("password", SALT_ROUND);
+  const userIDs = [];
+  for (let i = 1; i <= NUM_OF_VUSER; i++) userIDs.push(`user${i}`);
+  await Promise.all(
+    userIDs.map((id) => knex("user").insert({ id, hashed_password }))
+  );
 };
