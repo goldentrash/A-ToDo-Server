896fa5e9262528179ed354180aefca746d5b086a
White Piano <startoflateness@gmail.com>
2023-10-17 12:03:33 +0900

    매 시간에 한 번이 아닌, 매 분마다 실행되던 문제 해결

--- constants/notification.ts
+++ constants/notification.ts
@@ -1,7 +1,7 @@
 export const PUSH_CHANNEL_ID = "push";
 
-// GMT +9
-export const SCHEDULE_PUSH_WATING_ALARM = { hour: 23 };
-export const SCHEDULE_PUSH_OLD_WORKING_HINT = { hour: 11 };
+// GMT 0
+export const SCHEDULE_PUSH_WATING_ALARM = { hour: 23, minute: 0 };
+export const SCHEDULE_PUSH_OLD_WORKING_HINT = { hour: 11, minute: 0 };
 
 export const OLD_WORKING_BASE_TIME = 8;
