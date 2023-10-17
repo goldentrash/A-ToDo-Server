195133731b9bad12e4a3d9aee9d112578d6f274e
White Piano <startoflateness@gmail.com>
2023-10-17 11:53:06 +0900

    push log를 더 상세하게 변경

--- schedules/pushOldWorkingHint.ts
+++ schedules/pushOldWorkingHint.ts
@@ -9,34 +9,38 @@ import {
 } from "../constants";
 import { expo } from ".";
 
-rotatingStream.logInfo("Schedule Job pushOldWorkingHint");
-scheduleJob(SCHEDULE_PUSH_OLD_WORKING_HINT, async () => {
-  const query = knex("task")
-    .innerJoin("user", "task.user_id", "user.id")
-    .where("progress", "doing")
-    .whereNotNull("push_token")
-    .select("user_id", "push_token")
-    .select(
-      knex.raw("HOUR(TIMEDIFF(CURRENT_TIMESTAMP, `started_at`)) AS time_passed")
-    )
-    .having("time_passed", ">=", OLD_WORKING_BASE_TIME);
+const query = knex("task")
+  .innerJoin("user", "task.user_id", "user.id")
+  .where("progress", "doing")
+  .whereNotNull("push_token")
+  .select("user_id", "push_token")
+  .select(
+    knex.raw("HOUR(TIMEDIFF(CURRENT_TIMESTAMP, `started_at`)) AS time_passed")
+  )
+  .having("time_passed", ">=", OLD_WORKING_BASE_TIME);
 
+rotatingStream.logInfo(
+  `Schedule pushOldWorkingHint at every ${SCHEDULE_PUSH_OLD_WORKING_HINT}`
+);
+scheduleJob(SCHEDULE_PUSH_OLD_WORKING_HINT, async () => {
   const targetList = (await query) as unknown as {
     user_id: string;
     push_token: string;
     time_passed: number;
   }[];
+  rotatingStream.logInfo(
+    `Carry out pushOldWorkingHint(target found: ${targetList.length})`
+  );
+
   const messages: ExpoPushMessage[] = targetList.map(
     ({ user_id, push_token, time_passed }) => ({
       to: push_token,
-      title: `일이 잘 풀리지 않으시나요?`,
-      body: `${user_id}님이 일을 시작하신 지 ${time_passed}시간이 지났어요. 더 작은 일로 쪼개보는 건 어떠신가요?`,
+      title: `더 작은 일로 쪼개보시는 건 어떤가요?`,
+      body: `${user_id}님이 일을 시작하신지 ${time_passed}시간이 지났습니다.`,
       channelId: PUSH_CHANNEL_ID,
     })
   );
-
   await expo.sendPushNotificationsAsync(messages);
-
   for (const { to, title, body } of messages)
-    rotatingStream.logInfo(`Send [ ${title} - ${body} ] to ${to}`);
+    rotatingStream.logInfo(`Push [ ${title} - ${body} ] to ${to}`);
 });
