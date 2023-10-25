03a8cc5d8a301ecbd3d871ad51dced8aa6089ee9
goldentrash <startoflateness@gmail.com>
2023-10-25 22:03:16 +0900

    Doc/readme (#19)
    * service link 추가
    
    * 가독성 향상
    
    * docker imge update script 작성
    
    * server settup delay 고려

--- script/test_e2e.sh
+++ script/test_e2e.sh
@@ -6,7 +6,10 @@ node ./bin/www &
 PID=$!
 
 # run test
-yarn newman run -e test/local.postman_environment.json -d test/variables.json "test/functionality test.postman_collection.json"
+yarn newman run \
+  -e test/local.postman_environment.json \
+  -d test/variables.json \
+  "test/functionality test.postman_collection.json"
 TEST_RET=$?
 
 # exit server
