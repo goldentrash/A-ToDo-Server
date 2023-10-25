7e5b712846c7bc3ecd581ff318223132cc3c656b
goldentrash <startoflateness@gmail.com>
2023-10-25 22:03:22 +0900

    push log를 더 상세하게 변경 (#37)

--- schedules/pushWatingAlarm.ts
+++ schedules/pushWatingAlarm.ts
@@ -5,28 +5,36 @@ import { knex } from "../repositories";
 import { PUSH_CHANNEL_ID, SCHEDULE_PUSH_WATING_ALARM } from "../constants";
 import { expo } from ".";
 
-rotatingStream.logInfo("Schedule Job pushWatingAlarm");
-scheduleJob(SCHEDULE_PUSH_WATING_ALARM, async () => {
-  const query = knex("task")
-    .innerJoin("user", "task.user_id", "user.id")
-    .where("progress", "todo")
-    .whereNotNull("push_token")
-    .groupBy("user_id", "progress")
-    .select("user_id", "push_token")
-    .count("*", { as: "num_of_todo" });
+const query = knex("task")
+  .innerJoin("user", "task.user_id", "user.id")
+  .where("progress", "todo")
+  .whereNotNull("push_token")
+  .groupBy("user_id", "progress")
+  .select("user_id", "push_token")
+  .count("*", { as: "num_of_todo" });
 
-  const targetList = await query;
+rotatingStream.logInfo(
+  `Schedule pushWatingAlarm at every ${SCHEDULE_PUSH_WATING_ALARM}`
+);
+scheduleJob(SCHEDULE_PUSH_WATING_ALARM, async () => {
+  const targetList = (await query) as unknown as {
+    user_id: string;
+    push_token: string;
+    num_of_todo: number;
+  }[];
+  rotatingStream.logInfo(
+    `Carry out pushWatingAlarm(target found: ${targetList.length})`
+  );
+
   const messages: ExpoPushMessage[] = targetList.map(
     ({ user_id, push_token, num_of_todo }) => ({
-      to: push_token as string,
+      to: push_token,
       title: `오늘은 어떤 일을 해볼까요?`,
       body: `${user_id}님을 기다리는 ${num_of_todo}개의 일이 있습니다!`,
       channelId: PUSH_CHANNEL_ID,
     })
   );
-
   await expo.sendPushNotificationsAsync(messages);
-
   for (const { to, title, body } of messages)
-    rotatingStream.logInfo(`Send [ ${title} - ${body} ] to ${to}`);
+    rotatingStream.logInfo(`Push [ ${title} - ${body} ] to ${to}`);
 });
